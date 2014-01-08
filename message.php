52ab1fa9d0b9ea6304f6a37b83037a0b3006610f
kmschlagel <kmaillette@gmail.com>
2014-01-07 19:28:26 -0600

    Framed out and styled unordered list for Mary's recent posts.

--- message.php
+++ message.php
@@ -24,9 +24,20 @@
                 <p>651.773.9397</p>
                 <div class="map">
                 <iframe id="gmap" width="225" height="175" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=5399+Geneva+Avenue+North+Oakdale,+Minnesota+55128&amp;aq=&amp;sll=44.934383,-93.112741&amp;sspn=0.067201,0.169086&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=5399+Geneva+Ave+N,+Oakdale,+Minnesota+55128&amp;z=14&amp;ll=45.026986,-92.983909&amp;output=embed"></iframe>
-                <p><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=5399+Geneva+Avenue+North+Oakdale,+Minnesota+55128&amp;aq=&amp;sll=44.934383,-93.112741&amp;sspn=0.067201,0.169086&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=5399+Geneva+Ave+N,+Oakdale,+Minnesota+55128&amp;z=14&amp;ll=45.026986,-92.983909" target="_blank">View Larger Map</a></p>  
+                <p><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=5399+Geneva+Avenue+North+Oakdale,+Minnesota+55128&amp;aq=&amp;sll=44.934383,-93.112741&amp;sspn=0.067201,0.169086&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=5399+Geneva+Ave+N,+Oakdale,+Minnesota+55128&amp;z=14&amp;ll=45.026986,-92.983909" target="_blank"><span>View Larger Map</span></a></p>  
                </div> <!--end map-->
 			   </div> <!-- /findus -->
+               
+         <div id="posts">
+         	 <h4>Recent Posts</h4>
+         	<ul>
+            	<li><a href="#">"Title of Recent Post" <span>(1.12.14)</span></a></li>
+                <li><a href="#">"Title of Another Post" <span>(1.15.14)</span></a></li>
+                <li><a href="#">"Title of Another Recent Post" <span>(1.17.14)</span></a></li>
+                <li><a href="#">"Title of Another Post" <span>(1.20.14)</span></a></li>
+                <li><a href="#">"Another Post with a Longer Title" <span>(1.24.14)</span></a></li>
+            </ul>
+         </div>
                
     </div> <!--end sidebar-->
     	
